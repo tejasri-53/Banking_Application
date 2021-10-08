eae7cc1da2c045519640d20595ffa2bb8bd0ab4a
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-10-08 19:33:42 +0530

    Update and rename Transactions.cs to DisplayMessages.cs

---
+++ DisplayMessages.cs
@@ -0,0 +1,94 @@
+using System;
+
+
+namespace BankApplication
+{
+    class DisplayMessages
+    {
+        
+        public void Options()
+        {
+            Console.WriteLine("1. Create a Account");
+            Console.WriteLine("2. Deposit Amount");
+            Console.WriteLine("3. Withdraw Amount");
+            Console.WriteLine("4. Transfer Amount");
+            Console.WriteLine("5. Balance Enquiry");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("Select Your Choice: ");
+        }
+
+        public static string EnterUserName()
+        {
+            Console.Write("Enter Your Name:\n ");
+            return Console.ReadLine();
+        }
+        public static string EnterPIN()
+        {
+            Console.Write("Enter Your PIN: \n");
+            return Console.ReadLine();
+
+        }
+        public static int EnterAccountID()
+        {
+            Console.Write("Enter Your Account ID: \n");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static double EnterAmount()
+        {
+            Console.Write("Enter Amount To Deposit: \n");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static void InvalidPIN()
+        {
+            Console.WriteLine("Invalid PIN\n");
+        }
+
+        public static void AccountDoesntExist()
+        {
+            Console.WriteLine("Account Doesn't Exist\n");
+        }
+
+        public static void Invalid()
+        {
+            Console.WriteLine("Invalid Choice\n");
+        }
+        public static void Exit()
+        {
+            Console.WriteLine("Successfully Exited\n");
+        }
+
+
+        public static double EnterWithdrawAmount()
+        {
+            Console.Write("Enter Amount To Withdraw:\n ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static void InsufficientAmount()
+        {
+            Console.WriteLine("Insufficient Amount\n");
+        }
+
+
+        public static void DepositMessage()
+        {
+            Console.WriteLine("Successfully Deposited\n");
+        }
+        public static void WithDrawMessage()
+        {
+            Console.WriteLine(" Amount Successfully Withdrawn\n");
+        }
+        public static void TransferMessage()
+        {
+            Console.WriteLine("Successfull Transaction of Transfer");
+        }
+
+        public static double EnterTransferAmount()
+        {
+            Console.Write("Enter Amount To Transfer: ");
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
