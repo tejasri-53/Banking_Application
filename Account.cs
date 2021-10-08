ca996eff66020cbd33d10bd065d60f896bb23081
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-10-08 19:32:24 +0530

    Update Account.cs

--- Account.cs
+++ Account.cs
@@ -1,56 +1,44 @@
-﻿using System.Collections.Generic;
-using BankApplication.Services;
+using System;
+
 namespace BankApplication
 {
-    class Transfer
+    class Account
     {
-        
-        public static void TransferAmount(Dictionary<int, Account> AccountsList)
+        public static int index = 0;
+        public int AccountID;
+        public string name;
+        public double amount;
+        public string pin;
+
+
+        public Account(string name, string pin)
         {
-
-            int accountID = DisplayMessages.EnterAccountID();
-
-            if (AccountsList.ContainsKey(accountID))
-            {
-                string pin = DisplayMessages.EnterPIN();
-                if (!Bank.Validate(accountID, pin))
-                {
-                    DisplayMessages.InvalidPIN();
-                }
-                else
-                {
-                    int toID = DisplayMessages.EnterAccountID();
-                    if (AccountsList.ContainsKey(toID))
-                    {
-                        double amount = DisplayMessages.EnterTransferAmount();
-                        Account account = AccountsList[accountID];
-                        
-                        if (BankAccount.VerifyBalanceAmount(account, amount))
-                        {
-                            account.SetAmount(account.GetAmount() - amount);
-                            AccountsList[toID].SetAmount(AccountsList[toID].GetAmount() + amount);
-                            DisplayMessages.TransferMessage();
-                            BankAccount.BalanceEnquiry(account);
-
-                        }
-                        else
-                        {
-                            DisplayMessages.InsufficientAmount();
-                        }
-                    }
-                    else
-                    {
-                        DisplayMessages.AccountDoesntExist();
-                    }
-                }
-            }
-            else
-            {
-                DisplayMessages.AccountDoesntExist();
-            }
+            this.AccountID = index++;
+            this.pin = pin;
+            this.name = name;
+            this.amount = 0;
+            Console.WriteLine("Account Created Successfully with Account No: " + AccountID + " Name: " + name + " Balance: " + amount);
         }
 
-    }
+        public String GetPin()
+        {
+            return this.pin;
+        }
 
+
+        public int GetAccountID()
+        {
+            return this.AccountID;
+        }
+
+        public double GetAmount()
+        {
+            return this.amount;
+        }
+        public void SetAmount(double amount)
+        {
+            this.amount = amount;
+        }
+    }
 
 }
