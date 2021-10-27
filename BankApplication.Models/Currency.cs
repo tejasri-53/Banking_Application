b6d6451fd1422def042e4205e6bb2f9b928e1954
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-10-27 18:37:02 +0530

    Accepted Currency Model

---
+++ BankApplication.Models/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp.Models
+{
+    public class Currency
+    {
+        public string CurrencyCode { get; set; }
+        public double EquivalentINR { get; set; }
+        public Currency(string currencyCode, double equivalentINR)
+        {
+            CurrencyCode = currencyCode;
+            EquivalentINR = equivalentINR;
+        }
+
+    }
+}
