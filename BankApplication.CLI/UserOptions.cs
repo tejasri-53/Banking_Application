44f2763ce33eb635351e80af524b23332dd2f401
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-10-27 18:32:55 +0530

    User Choices

---
+++ BankApplication.CLI/UserOptions.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace BankApp.CLI
+{
+    public class Choices
+    {
+        public enum MainChoice
+        {
+            SetupBank=1,
+            CreateStaffAccount,
+            Login ,
+            EXIT,
+        }
+
+        public enum CustomerLoginChoice
+        {
+            TransferMoney = 1,
+            Deposit,
+            Withdraw,
+            ShowTransactions,
+            RevertTransaction,
+            Logout,
+        }
+
+        public enum StaffLoginChoice
+        {
+            CreateAccount = 1,
+            UpdateAccount,
+            DeleteAccount,
+            UpdatesRTGS,
+            UpdatesIMPS,
+            UpdatedRTGS,
+            UpdatedIMPS,
+            ViewAccountTransaction,
+            Logout,
+            
+        }
+
+        public enum UpdateCustomerAccountChoice
+        {
+            UpdateName = 1,
+            UpdatePassword,
+            Back,
+        }
+    }
+}
