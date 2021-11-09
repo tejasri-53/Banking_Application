5e24159fad729179b7753bf4200fc7eae6cbb2a7
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-11-09 10:41:37 +0530

    Add files via upload

---
+++ BankApp.Models/Currency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.IO;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Net.Http;
+using System.Net;
+
+
+namespace BankApp.Models
+{
+    public class Currency
+    {
+        public Dictionary<string, float> currency = new Dictionary<string, float>();
+        
+
+    }
+}
+
+
---
+++ BankApp.Models/Transaction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.Model
+{
+    public class Transaction
+    {
+        public string transactionID;
+        public string sID, rID;
+        public double amount;
+        public string description;
+        public string  time;
+        public Transaction(string tID, string sID, string rID, string description,double amount,  string time)
+        {
+            this.transactionID = tID;
+            this.sID = sID;
+            this.rID = rID;
+            this.description = description;
+            this.amount = amount;
+            this.time = time;
+        }
+        public Transaction(string tID, string rID, double amount, string description, string time)
+        {
+            this.transactionID = tID;
+            this.rID = rID;
+            this.amount = amount;
+            this.description= description;
+            this.time = time;
+        }
+        
+    }
+}
