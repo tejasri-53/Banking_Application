a2e5b36aa0db0424db8e9e98bb5d44e62701c036
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-11-30 12:54:46 +0530

    Add files via upload

---
+++ BankApp.Services/SqlQueries.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp.Services
+{
+    class SqlQueries
+    {
+        
+        public static string CheckTabelsExist = "SELECT count(*) FROM information_schema.tables WHERE table_schema = 'bankapp' AND table_name = 'banks' LIMIT 1;";
+
+
+        public static string CreateDatabase = "CREATE DATABASE bankapp;";
+        public static string CreateBanksTable = "CREATE TABLE `bankapp`.`banks` (`BankId` VARCHAR(45) NOT NULL,`BankName` VARCHAR(45) NULL,`sRTGSCharge` FLOAT NULL DEFAULT 0,`sIMPSCharge` FLOAT NULL DEFAULT 5,`oRTGSCharge` FLOAT NULL DEFAULT 2,`oIMPSCharge` FLOAT NULL DEFAULT 6,PRIMARY KEY(`BankId`));";
+        public static string CreateCustomerAccountsTable = "CREATE TABLE `bankapp`.`customeraccounts` ( `AccountId` VARCHAR(45) NOT NULL, `BankId` VARCHAR(45) NOT NULL, `Balance` FLOAT NOT NULL, `Name` VARCHAR(45) NOT NULL, `Password` VARCHAR(45) NOT NULL, `IsActive` TINYINT NOT NULL DEFAULT 1, PRIMARY KEY (`AccountId`), INDEX `Customer BankId_idx` (`BankId` ASC) VISIBLE, CONSTRAINT `Customer BankId` FOREIGN KEY (`BankId`) REFERENCES `bankapp`.`banks` (`BankId`) ON DELETE NO ACTION ON UPDATE NO ACTION);";
+        public static string CreateStaffAccountsTable = "CREATE TABLE `bankapp`.`staffaccounts` ( `AccountId` VARCHAR(45) NOT NULL, `BankId` VARCHAR(45) NOT NULL, `Name` VARCHAR(45) NOT NULL, `Password` VARCHAR(45) NOT NULL, PRIMARY KEY (`AccountId`), INDEX `staff and bankId link_idx` (`BankId` ASC) VISIBLE, CONSTRAINT `staff and bankId link` FOREIGN KEY (`BankId`) REFERENCES `bankapp`.`banks` (`BankId`) ON DELETE NO ACTION ON UPDATE NO ACTION);";
+        public static string CreateTransactionsTable = "CREATE TABLE `bankapp`.`transactions` ( `TransactionId` VARCHAR(45) NOT NULL, `Amount` FLOAT NULL, `Type` INT NULL, `Time` VARCHAR(45) NULL, `SenderId` VARCHAR(45) NULL, `ReceiverId` VARCHAR(45) NULL, PRIMARY KEY (`TransactionId`), INDEX `SenderId_idx` (`SenderId` ASC) VISIBLE, INDEX `ReceiverId_idx` (`ReceiverId` ASC) VISIBLE, CONSTRAINT `SenderId` FOREIGN KEY (`SenderId`) REFERENCES `bankapp`.`customeraccounts` (`AccountId`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `ReceiverId` FOREIGN KEY (`ReceiverId`) REFERENCES `bankapp`.`customeraccounts` (`AccountId`) ON DELETE NO ACTION ON UPDATE NO ACTION);";
+        public static string CreateCurrencyTable = "CREATE TABLE `bankapp`.`currency` ( `currency` VARCHAR(4) NOT NULL, `BankId` VARCHAR(45) NULL, PRIMARY KEY (`currency`), INDEX `bank and currency link_idx` (`BankId` ASC) VISIBLE, CONSTRAINT `bank and currency link` FOREIGN KEY (`BankId`) REFERENCES `bankapp`.`banks` (`BankId`) ON DELETE NO ACTION ON UPDATE NO ACTION);";
+
+
+        
+        public static string GetBalance = "SELECT `Balance` from `bankapp`.`customeraccounts` where AccountId = '{0}';";
+        public static string GetName = "SELECT `Name` from `bankapp`.`customeraccounts` where AccountId = '{0}';";
+        public static string GetBankId = "SELECT `BankId` from `bankapp`.`customeraccounts` where AccountId = '{0}';";
+        public static string GetCurrencyValue = "SELECT `Value` from `bankapp`.`currency` where currency = '{0}'";
+        public static string GetTransactions = "SELECT * from `bankapp`.`transactions` WHERE ReceiverId = '{0}' or SenderId = '{1}';";
+        public static string GetBanksRTGSCharges = "SELECT sRTGSCharge from `bankapp`.`banks` where BankId = '{0}';";
+        public static string GetBanksIMPSCharges = "SELECT sIMPSCharge from `bankapp`.`banks` where BankId = '{0}';";
+        public static string GetBankoRTGSCharges = "SELECT oRTGSCharge from `bankapp`.`banks` where BankId = '{0}';";
+        public static string GetBankoIMPSCharges = "SELECT oIMPSCharge from `bankapp`.`banks` where BankId = '{0}';";
+        public static string GetTransactionAmount = "SELECT Amount from `bankapp`.`transactions` WHERE TransactionId = '{0}'";
+        public static string GetTransactionType = "SELECT Type from `bankapp`.`transactions` WHERE TransactionId = '{0}'";
+        public static string GetTransactionSenderId = "SELECT SenderId from `bankapp`.`transactions` WHERE transactionId = '{0}';";
+        public static string GetTransactionReceiverId = "SELECT ReceiverId from `bankapp`.`transactions` WHERE transactionId = '{0}';";
+
+
+        public static string InsertIntoCustomersTable = "INSERT INTO `bankapp`.`customeraccounts`(`AccountId`,`BankId`,`Balance`,`Name`,`Password`)VALUES('{0}', '{1}', {2}, '{3}', '{4}');";
+        public static string InsertIntoStaffsTable = "INSERT INTO `bankapp`.`staffaccounts`(`AccountId`,`BankId`,`Name`,`Password`)VALUES('{0}', '{1}', '{2}', '{3}');";
+        public static string InsertIntoBanksTable = "INSERT INTO `bankapp`.`banks`(`BankId`,`BankName`,`sRTGSCharge`,`sIMPSCharge`,`oRTGSCharge`,`oIMPSCharge`)VALUES('{0}', '{1}', {2}, {3}, {4}, {5}); ";
+        public static string AddCurrency = "INSERT INTO `bankapp`.`currency` (`currency`, `BankId`, `value`) VALUES ('{0}', '{1}', {2});";
+        public static string InsertTransactionReceiver = "INSERT INTO `bankapp`.`transactions` (`TransactionId`, `Amount`, `Type`, `Time`, `ReceiverId`) VALUES ('{0}', {1}, {2}, '{3}', '{4}');";
+        public static string InsertTransaction = "INSERT INTO `bankapp`.`transactions` (`TransactionId`, `Amount`, `Type`, `Time`, `SenderId`, `ReceiverId`) VALUES ('{0}', {1}, {2}, '{3}', '{4}', '{5}');";
+
+
+        public static string DeleteTransaction = "DELETE FROM `bankapp`.`transactions` WHERE TransactionId = '{0}';";
+        public static string DeleteCustomerAccount = "UPDATE `bankapp`.`customeraccounts` SET  `IsActive` = 0 WHERE `AccountId` = '{0}';";
+
+
+        public static string UpdateBalance = "UPDATE `bankapp`.`customeraccounts` SET  `Balance` = {0} WHERE `AccountId` = '{1}';";
+        public static string UpdatePassword = "UPDATE `bankapp`.`customeraccounts` SET `Password` = '{0}' WHERE `AccountId` = '{1}';";
+        public static string UpdateName = "UPDATE `bankapp`.`customeraccounts` SET `Name` = '{0}' WHERE `AccountId` = '{1}';";
+        public static string UpdatesRTGS = "UPDATE `bankapp`.`banks` SET `sRTGSCharge` = {0} WHERE `BankId` = '{1}';";
+        public static string UpdatesIMPS = "UPDATE `bankapp`.`banks` SET `sIMPSCharge` = {0} WHERE `BankId` = '{1}';";
+        public static string UpdateoRTGS = "UPDATE `bankapp`.`banks` SET `oRTGSCharge` = {0} WHERE `BankId` = '{1}';";
+        public static string UpdateoIMPS = "UPDATE `bankapp`.`banks` SET `oIMPSCharge` = {0} WHERE `BankId` = '{1}';";
+        
+
+
+        public static string AuthenticateCustomer = "SELECT EXISTS(SELECT 1 FROM `bankapp`.`customeraccounts` WHERE AccountId = '{0}');";
+        public static string AuthenticateStaff = "SELECT EXISTS(SELECT 1 FROM `bankapp`.`staffaccounts` WHERE AccountId = '{0}');";
+    }
+}
---
+++ BankApp.Services/TransactionService.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.IO;
+using System.Threading.Tasks;
+using BankApp.Models;
+using BankApp.Models.Exceptions;
+using MySql;
+using BankApp.Services;
+using MySql.Data.MySqlClient;
+using ConsoleTables;
+using System.Transactions;
+
+namespace BankApp.Services
+{
+    public class TransactionService
+    {
+        
+        public static string GetDateTimeNow(bool forId)
+        {
+            return forId ? DateTime.Now.ToString("ddMMyyyyHHmmss") : DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+        public static string DepositAmount(string accountId, float amount)
+        {
+            try
+            {
+                float currentBalance = AccountService.GetBalance(accountId);
+                float newBalance = currentBalance +amount;
+
+                using (MySqlConnection conn = new MySqlConnection(BankService.connStr))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(String.Format(SqlQueries.UpdateBalance, newBalance, accountId), conn))
+                    {
+                        cmd.Connection.Open();
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        
+                        return AccountService.GetName(accountId);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+        public static bool WithdrawAmount(string accountId, float amount)
+        {
+            try
+            {
+                float newBalance = AccountService.GetBalance(accountId) - amount;
+                if (newBalance < 0)
+                {
+                    return false;
+                }
+                using (MySqlConnection conn = new MySqlConnection(BankService.connStr))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(String.Format(SqlQueries.UpdateBalance, newBalance, accountId), conn))
+                    {
+                        cmd.Connection.Open();
+                        MySqlDataReader reader = cmd.ExecuteReader();
+                        
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        
+
+        public static bool TransferAmountRTGS(string fromId, string toId, float amount)
+        {
+            float UpdatedAmount;
+            string fromBankId = BankService.GetBankId(fromId);
+
+            if (AccountService.GetBalance(fromId) - amount < 0)
+            {
+                return false;
+            }
+            if (fromBankId == BankService.GetBankId(toId))
+            {
+                float temp = amount * (BankService.GetBanksRTGSCharges(fromBankId) / 100);
+                UpdatedAmount = amount - temp;
+            }
+            else
+            {
+                float temp = amount * (BankService.GetBankoRTGSCharges(fromBankId) / 100);
+                UpdatedAmount = amount - temp;
+            }
+
+            
+
+            AccountService.UpdateBalance(fromId, AccountService.GetBalance(fromId) - amount);
+            AccountService.UpdateBalance(toId, AccountService.GetBalance(toId) + UpdatedAmount);
+
+            return true;
+        }
+
+        public static bool TransferAmountIMPS(string fromId, string toId, float amount)
+        {
+            float UpdatedAmount;
+            string fromBankId = BankService.GetBankId(fromId);
+
+            if (AccountService.GetBalance(fromId) - amount < 0)
+            {
+                return false;
+            }
+            if (fromBankId == BankService.GetBankId(toId))
+            {
+                float temp = amount * (BankService.GetBanksIMPSCharges(fromBankId) / 100);
+                UpdatedAmount = amount - temp;
+            }
+            else
+            {
+                float temp = amount * (BankService.GetBankoIMPSCharges(fromBankId) / 100);
+                UpdatedAmount = amount - temp;
+            }
+
+            
+
+            AccountService.UpdateBalance(fromId, AccountService.GetBalance(fromId) - amount);
+            AccountService.UpdateBalance(toId, AccountService.GetBalance(toId) + UpdatedAmount);
+
+            
+
+            return true;
+        }
+        
+
+    }
+}
