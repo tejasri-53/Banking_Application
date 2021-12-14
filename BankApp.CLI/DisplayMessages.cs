76abaa6c98ef0cd1cd8d79a4a92ac058b9652313
tejasri-53 <91427712+tejasri-53@users.noreply.github.com>
2021-12-14 10:38:29 +0530

    Update DisplayMessages.cs

--- BankApp.CLI/DisplayMessages.cs
+++ BankApp.CLI/DisplayMessages.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,94 +8,60 @@ namespace BankApp.CLI
 {
     class Messages
     {
-        public static void WelcomeMenu() 
-        {
-            Console.WriteLine("Choose an option...\n1)Add Bank \n 2)Create Staff Account \n 3)Login\n4) EXIT\n\nEnter your choice: "); 
-        }
-        public static void StaffLoginMenu()
-        {
-            Console.WriteLine("Choose an action...\n1) Create Account\n2) Update Account\n3) Delete Account\n4) Update sRTGS\n5) Update sIMPS\n6) Update oRTGS\n7) Update oIMPS\n8) Logout\n\nEnter your choice: ");
-        }
-        public static void TransactionModeMenu()
-        {
-            Console.WriteLine("Choose the type of transcation you want to perform: \n1)RTGS\n2)IMPS\n\nEnter your choice:");
-        }
-        public static void UpdateCustomerAccount()
-        {
-            Console.WriteLine("Choose an action...\n1) Update Name\n2) Update Password\n3) Back\n\nEnter your choice:");
-        }
-        public static void AskName()
-        {
-            Console.WriteLine(" Enter name: ");
-        }
+        public static string WelcomeMenu = "Choose an option...\n1)Create Bank \n2) Login\n3) EXIT\n\nEnter your choice: ";
 
-        public static void AskPassword()
-        {
-            Console.WriteLine(" Enter Password:");
-        }
 
-        public static void AskAccountId()
-        {
-            Console.WriteLine(" Enter the AccountId :");
-        }
-        public static void AskDepositAmount()
-        {
-            Console.WriteLine(" Enter the amount to be deposited: ");
-        }
+        public static string StaffLoginMenu = "Choose an action...\n1) Create Account\n2) Update Account\n3) Delete Account\n4) Update sRTGS\n5) Update sIMPS\n6) Update oRTGS\n7) Update oIMPS\n 8) Logout\nEnter your choice: ";
 
-        public static void AskWithdrawAmount()
-        {
-            Console.WriteLine(" Enter the amount to withdraw: ");
-        }
 
-        public static void InvalIdAccountId()
-        {
-            Console.WriteLine(" InvlId Account Id !!! ");
-        }
+        public static string UpdateCustomerAccount = "Choose an action...\n1) Update Name\n2) Update Password\n3) Back\n\nEnter your choice: ";
 
-        public static void LoginMenu()
-        {
-            Console.WriteLine(" \n\n1)Deposit  \n 2)Transfer Money (INR only)\n3) Withdraw Money (INR only)\n4) Logout\n\nChoose an option:");
-        }
 
-        public static void InvalIdCredentials()
-        {
-            Console.WriteLine(" Wrong Id or Password... ");
-        }
+        public static string AskName = "Enter name: ";
 
-        public static void TransferAskId()
-        {
-            Console.WriteLine("Enter the Id of the account to whome u want to transfer ammount: ");
-        }
+        public static string TransactionModeMenu = "Choose the type of transcation you want to perform: \n1)RTGS\n2)IMPS\n\nEnter your choice: ";
 
-        public static void AskTransferAmount()
-        {
-            Console.WriteLine("Enter the amount to be transfered: ");
-        }
-        public static string TransactionSuccess()
-        {
-            return ("Transaction sucessfully completed !!! ");
-        }
 
-        public static string  TransactionErrorInsufficientBal()
-        {
-            return ("Unable to complete transaction as there is insufficient balance. ");
-        }
+        public static string AskPassword = "Enter Password: ";
 
-        public static void InsuffiecientFunds()
-        {
-            Console.WriteLine("Insufficient Funds.....");
-        }
-        public static void WithdrawError()
-        {
-            Console.WriteLine("An error occured while withdrawing...");
-        }
 
-        public static void TransactionFetchingError()
-        {
-            Console.WriteLine("An error occured while fetching Transactions...");
-        }
+        public static string AskAccountId = "Enter the AccountId : ";
 
+
+        public static string AskDepositAmount = "Enter the amount to be deposited: ";
+
+
+        public static string AskWithdrawAmount = "Enter the amount to withdraw: ";
+
+
+        public static string InvalIdAccountId = "InvlId Account Id !!!";
+
+
+        public static string LoginMenu = "\n\n1) Transfer Money \n2) Withdraw Money \n3)Deposit\n4) Logout\n\nChoose an option:";
+
+
+        public static string InvalIdCredentials = "Wrong Id or Password...";
+
+
+        public static string TransferAskId = "Enter the Id of the account to whome u want to transfer ammount: ";
+
+
+        public static string AskTransferAmount = "Enter the amount to be transfered: ";
+
+
+        public static string TransactionSuccess = "Transaction sucessfully completed !!!";
+
+
+        public static string TransactionErrorInsufficientBal = "Unable to complete transaction as there is insufficient balance.";
+
+
+        public static string InsuffiecientFunds = "Insufficient Funds.....";
+
+
+        public static string WithdrawError = "An error occured while withdrawing...";
+
+
+        public static string TransactionFetchingError = "An error occured while fetching Transactions...";
 
     }
 }
